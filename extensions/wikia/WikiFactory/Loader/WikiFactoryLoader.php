1b242e0403e9b2e0c75bb087ca44b8788293cf45
mech <wozniak.jacek@gmail.com>
2018-03-09 14:21:26 +0100

    MAIN-13125: don't append title parameter when doing the NotPrimary redirect

--- extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
+++ extensions/wikia/WikiFactory/Loader/WikiFactoryLoader.php
@@ -435,8 +435,16 @@ class WikiFactoryLoader {
 			$redirectUrl = WikiFactory::getLocalEnvURL( $this->mCityUrl );
 			$target = rtrim( $redirectUrl, '/' ) . '/' . $this->pathParams;
 
-			if ( !empty( $_GET ) ) {
-				$target .= '?' . http_build_query( $_GET );
+			// skip the 'title' which is part of the $target, but append remaining parameters
+			$queryParams = array_filter(
+				$_GET,
+				function ($key) {
+					return $key !== 'title';
+				},
+				ARRAY_FILTER_USE_KEY
+			);
+			if ( !empty( $queryParams ) ) {
+				$target .= '?' . http_build_query( $queryParams );
 			}
 
 			header( "X-Redirected-By-WF: NotPrimary" );
